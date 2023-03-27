33ebe1eab5063090eb12750e7c8970a425f4789a
deivissonss <deivisson.santos@detran.ba.gov.br>
2023-03-27 10:08:40 -0300

    [FEATURE] Remoção das páginas antigas e inclusão da view home

--- AppMyHomeLibrary/src/app/app-routing.module.ts
+++ AppMyHomeLibrary/src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'views/inicio',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'views/inicio',
+    loadChildren: () => import('./views/inicio/inicio.module').then( m => m.InicioPageModule)
   }
 ];
 
--- AppMyHomeLibrary/src/app/app.component.ts
+++ AppMyHomeLibrary/src/app/app.component.ts
@@ -6,13 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Inicio', url: '/views/inicio', icon: 'home' },
+    { title: 'Sair', url: '', icon: 'log-out' }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  
   constructor() {}
 }
